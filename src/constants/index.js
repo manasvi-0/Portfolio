754fd53654c019ec77c301517a8d0a5f62bcaf21
Manasvi <147179345+manasvi-0@users.noreply.github.com>
2026-01-15 08:25:23 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -207,16 +207,24 @@ export const myProjects = [
   },
 ];
 
-export const calculateSizes = (isSmall, isMobile, isTablet) => {
-  return {
-    deskScale: isSmall ? [3,3,3] : isMobile ? [4,4,4] : [6,6,6],
-    deskPosition: isSmall ? [-0.1, 1.7, -8.9] : isMobile ? [-0.1, 1.7, -8.9] : isTablet ? [0.1,-2.5,7.1] : [0.1,-9.5,0.1],
-    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-    ringPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
-    targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
-  };
-};
+export const calculateSizes = ({ isSmall, isMobile, isTablet }) => ({
+  deskScale: isSmall
+    ? [2.8, 2.8, 2.8]
+    : isMobile
+    ? [3.6, 3.6, 3.6]
+    : isTablet
+    ? [4.8, 4.8, 4.8]
+    : [6, 6, 6],
+
+  deskPosition: isSmall
+    ? [0, 1.5, -9.5]
+    : isMobile
+    ? [0, 1.6, -9]
+    : isTablet
+    ? [0, -1.8, -2]
+    : [0, -9.5, 0],
+});
+
 
 
 // Re-export carousel data so imports from "../constants" can access STRIP_* and sizing
